92949448c62bcc0a86df9381b2f23525fe291ad4
designstudio_2 <chris.alwin@me.com>
2014-04-11 15:54:45 +0100

    finished and validated and commented a few items

--- assets/js/assessmentScript/scripts.js
+++ assets/js/assessmentScript/scripts.js
@@ -13,9 +13,9 @@ showItem = function(currId) {
   return false;
 };
 
-hideItems = function(currEles, eleClass) {
+hideItems = function(currEle, eleClass) {
   var elements, i, _i, _len;
-  elements = document.getElementsByTagName(currEles);
+  elements = document.getElementsByTagName(currEle);
   for (_i = 0, _len = elements.length; _i < _len; _i++) {
     i = elements[_i];
     if (i.className === eleClass) {
